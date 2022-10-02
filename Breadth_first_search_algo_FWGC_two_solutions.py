b1a7cd4b0fe95dc99774e31f4c687e3281073387
Juan-Rubiales-Academic <113774572+Juan-Rubiales-Academic@users.noreply.github.com>
2022-10-02 16:04:06 +0200

    Create Breadth_first_search_algo_FWGC_two_solutions.py

---
+++ Breadth_first_search_algo_FWGC_two_solutions.py
@@ -0,0 +1,49 @@
+# Requiered code from Breadth_first_search_algo_FWGC.py
+
+# this algorithm will find two possible solutions
+def search2(problem, start):
+  first1 = []
+  path1 = []
+  path2 = []  
+  frontier2 = [(start,[start])]
+  reached2 = []
+  solution = False
+
+  while solution == False:
+    parent, path = frontier2.pop(0)
+    reached2.append(parent)
+
+    for child in expand(parent):
+
+        if len(child['l']) == 0:
+          # Put the condition of first solution reached and verify not the same end
+          if len(first1) == 0:
+            first1.append(child)
+            path1 = path + [child]
+            # return the two paths 
+          elif first1[0] != child:
+            path2 = path + [child]   
+            return path1, path2
+        
+        # Add to the queue and array (frontier2 and reached2) if child has not been visited
+        if child not in reached2:
+          frontier2.append((child, path + [child]))
+          reached2.append(child)
+
+  return solution
+
+
+start = transform_initial(problem["initial-state"])
+result = search2(problem, start)
+
+final_path1 = result[0]
+final_path2 = result[1]
+
+anwser1 = transform_last(start, final_path1)
+anwser2 = transform_last(start, final_path2)
+
+print(anwser1,"\n \n",anwser2)
+
+# The funciton 'submit_anwser' does not work if all the letters are not in - 
+# the exact postion even if they are in the same state
+#'fWGC,-WC,fG-fWC,G-W,fCG-fWG,C-G,fWC-fG,WC-,fWGC'
